636f42c5a6b5538fd54d31fbe1bced89fe7baf08
ParkGiraffe <op5321@soongsil.ac.kr>
2021-07-08 11:37:32 +0900

    feat : 클래스와 구조체의 생성자에 대한 학습 내용 추가

--- swift/swift_study/7_class_structure.swift
+++ swift/swift_study/7_class_structure.swift
@@ -76,3 +76,27 @@ coffee1.name = "아이스 아메리카노" // coffee1의 이름을 바꿔도
 coffee2.name             // coffee2는 완전히 별개이기 때문에 이름이 바뀌지 않는다. (nil)이 출력됨.
 
 
+
+// 2. 생성자 (Initializer)
+// class와 struct 모두 생성자를 가지고 있다. 생성자에서 초기값을 지정할 수 있다.
+
+class Dog2 {
+    var name: String?  //속성이 옵셔널이 아니라면 항상 초기값을 가져야 한다.
+    var age: Int       //그렇지 않다면 컴파일 에러가 발생한다.
+    var breed: String = "doge" // 속성을 정의할 때 초기값을 지정해줄 수도 있다.
+    
+    init() {
+        self.age = 0 // 새성자에서 age의 초기값을 0으로 설정
+    }
+}
+
+struct Coffee2 {
+    var name: String?
+    var size: String?
+    
+    init() {
+        self.size = "Tall"
+    }
+}
+
+
