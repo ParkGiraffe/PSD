96368161871380c1b93911c647948b636a815f81
ParkGiraffe <op5321@soongsil.ac.kr>
2021-07-16 20:38:21 +0900

    feat : tuple 파라미터 변경 관련 내용 추가

--- swift/swift_study/8.tuple.swift
+++ swift/swift_study/8.tuple.swift
@@ -11,8 +11,13 @@ import Foundation
 
 // 튜플은 어떠한 값들의 묶음을 말한다. 배열과 비슷한데, 배열과는 다르게 길이가 고정되어 있다.
 // 값에 접근할 때 []가 이나라 '.'을 사용한다.
-
 var coffeInfo = ("아이스 아메리카노", 5100)
-coffeInfo.0 // 아메리카노
+coffeInfo.0 // 아이스 아메리카노
 coffeInfo.1 // 5100
 c0ffeInfo.1 = 5200 // 재설정
+
+// 튜플의 파라미터에 이름을 붙일 수도 있다.
+var namedCoffeeInfo = (coffee: "아이스 아메리카노", price: 5100)
+namedCoffeeInfo.coffee // 아이스 아메리카노
+namedCoffeeInfo.price // 5100
+namedCoffeeInfo.price = 5200 // 5200으로 재설정
