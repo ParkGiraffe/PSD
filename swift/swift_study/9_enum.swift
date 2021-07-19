f3f4de379cf35533c6036eff0c5694f4fa26ee5b
ParkGiraffe <op5321@soongsil.ac.kr>
2021-07-19 12:40:57 +0900

    feat : String 값을 rawValue로 가지는 enum에 대한 내용 추가

--- swift/swift_study/9_enum.swift
+++ swift/swift_study/9_enum.swift
@@ -10,6 +10,7 @@ import Foundation
 // 1. Enum(Enumeration): 열거, 열거형
 
 // 1월부터 12월까지를 enum으로 정의
+// upper camel case 를 사용
 enum Month: Int { // Int를 원시값(rawValue)을 가지도록 정의
     case january = 1 // 따라서 각 케이스들은 1부터 12가지의 값을 가지고 있음.
     case februray
@@ -66,4 +67,9 @@ print(october) // Optional(Month.october)
 // Month(rawValue:)의 값이 옵셔널인 이유는, Enum에서 정의되지 않은 원시값을 가지고 생성할 경우 nil을 반환하기 때문이다.
 Month(rawValue: 15) // nil
 
-//
+// 일반적으로 enum은 Int만을 원시값으로 가질 수 있다고 생각한다. 왜냐하면 다른 프로그래밍 언어에서 그러하기 때문이다.
+// Swift의 enum은 조금 다른데, String을 원시값으로 가진다.
+enum IssueState: String {
+    case open = "open"
+    case closed = "closed"
+}
