821812b658024d0ec406e6ceddd9973924903bb8
ParkGiraffe <op5321@soongsil.ac.kr>
2021-07-25 20:20:30 +0900

    feat : 개인 공부 내용 추가

--- JS/1_변수와_상수.js
+++ JS/1_변수와_상수.js
@@ -4,8 +4,20 @@
 
 //변수 (변할수 있는 값을 저장) let
 //상수 (변하지 않는 값을 저장) const
+//ES6 이전에는 주로 var이라는 변수 선언을 사용했다.
+//lowerCamelCase를 사용
 
-//a라고 하는 변수를 선언
+//JS의 자료형
+//Number: Any number, including numbers with decimals: `4`, `8`, `1516`, `23.42`.
+//String: Any grouping of characters on your keyboard (letters, numbers, spaces, symbols, etc.) surrounded by single quotes: `' ... '` or double quotes `" ... "`. Though we prefer single quotes. Some people like to think of string as a fancy word for text.
+//Boolean: This data type only has two possible values— either `true` or `false` (without quotes). It’s helpful to think of booleans as on and off switches or as the answers to a “yes” or “no” question.
+//Null: This data type represents the intentional absence of a value, and is represented by the keyword `null` (without quotes).
+//Undefined: This data type is denoted by the keyword `undefined` (without quotes). It also represents the absence of a value though it has a different use than `null`.
+//Symbol: A newer feature to the language, symbols are unique identifiers, useful in more complex coding. No need to worry about these for now.
+//Object: Collections of related data.
+
+
+//a라고 하는 변수를 선언. 값이 없어도 내용을 추가할 수 있다.
 let a
 
 //a라는 변수에 값을 대입
@@ -18,11 +30,23 @@ a = 300
 const b = 200
 
 //b에다가 새 값을 대입 시도 (오류)
+// + const는 undefined값으로 설정할 수 없다. 무조건 value가 필요하다.
 b = 300
 
 //콘솔에서 변수, 상수 값을 확인
 console.log('a: ',a)
 console.log('b: ',b)
+
+// backticks를 감싸서 String Interpolation을 사용할 수 있다.
+console.log(`a: ${a}`) // a: 300
+
+// typeof Operator
+// 변수의 자료형을 검사할 때 사용한다.
+const unknown1 = 'yo';
+console.log(typeof unknown1); // Output: string
+
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
 
 
 // 변수와 상수에 대한 타입은 크게 2개 (원시 / 객체) 로 구분된다
@@ -77,6 +101,7 @@ console.log('obj2.name: ',obj2.name) //Steve
 //call-by-value (copy-by-value): 값이 그대로 복사 (서로 다른 메모리 공간 점유)
 
 /* array (배열) */
+// '[]'로 감싸서 만든다.
 let arr  = ['hello','there']
 let arr2 = arr
 
@@ -85,8 +110,32 @@ console.log('arr2:', arr[1])  //arr2: there
 
 arr2[2] = 'nice to meet you'
 console.log('arr:', arr[2])   //arr: nice to meet you
-
 //배열도 객체와 마찬가지로 메모리 공간을 참조하기 때문에 arr를 arr2에 대입한 이후 동일한 곳을 바라보는것을 확인할 수 있다
+
+// JS의 배열은 어떤 타입이든 저장할 수 있다. 심지어 다른 타입끼리도 한 배열에 저장할 수 있다.
+let egArr1 = ['example', 10, true]
+
+// let으로 선언된 array일 경우, 배열 자체를 변경할 수 있다.
+// const로 선언된 array일 경우, 배열 자체를 변경할 수 없다. 다만 그 안에 elements들을 수정할 수는 있다.
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+
+condiments [0] = 'Mayo';
+console.log (condiments); 
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils [3] =  'Spoon';
+console.log(utensils);
+
+/* 출력문 
+[ 'Mayo', 'Mustard', 'Soy Sauce', 'Sriracha' ]
+[ 'Mayo' ]
+[ 'Fork', 'Knife', 'Chopsticks', 'Spoon' ]
+*/
+
 
 /* function (함수) */
 function testFunc(a, b) {
@@ -100,3 +149,5 @@ console.log('testFunc: ', testFunc(3, 5))            //testFunc: 8
 console.log('funcContainer: ', funcContainer(3, 5))  //funcContainer: 8
 
 //함수 또한 객체, 배열과 마찬가지로 객체 타입으로 값의 복사는 '참조' 형태로 이루어진다
+
+
