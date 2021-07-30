40b17fd2682882e747191210ee9a77a32874605b
ParkGiraffe <op5321@soongsil.ac.kr>
2021-07-31 00:10:52 +0900

    feat : Control Flow 장 학습 완료

--- swift/swift_study/swift_official_docs/GuidedTour.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ swift/swift_study/swift_official_docs/GuidedTour.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -17,16 +17,32 @@ print(teamScore)
 //:
 //: You can use `if` and `let` together to work with values that might be missing. These values are represented as optionals. An optional value either contains a value or contains `nil` to indicate that a value is missing. Write a question mark (`?`) after the type of a value to mark the value as optional.
 //:
-// optional value는 ?를 이용해서 사용한다.
+// optional value는 ?를 이용해서 사용한다. 값이 있을 수도 있고 없을 수도 있음을 의미한다.
 
 var optionalString: String? = "Hello"
-print(optionalString == nil)
+print(optionalString == nil) // false
+var optionalString2: String? // nil
+print(optionalString2 == nil) // true
 
 var optionalName: String? = "John Appleseed"
 var greeting = "Hello!"
-if let name = optionalName {
+if let name = optionalName { // if 다음에 let을 쓰고, 옵셔널로 선언된 변수를 value로 선언한다.
     greeting = "Hello, \(name)"
 }
+
+var optionalName2: String?
+if let name = optionalName2 { // if 문에서 optional을 쓸 때, 값이 nil일 경우 false로 인식하고 스킵한다.
+    greeting = "Hello, \(name)"
+} else {
+    greeting = "Your name is empty" // nil일 경우 else문이 실행된다.
+}
+
+//if let name: String? = optionalName3 {
+//    greeting = "Hello, \(name)"
+//}
+// if 문에서 옵셔널 선언은 불가능
+
+
 
 //: - Experiment:
 //: Change `optionalName` to `nil`. What greeting do you get? Add an `else` clause that sets a different greeting if `optionalName` is `nil`.
@@ -38,18 +54,24 @@ if let name = optionalName {
 let nickname: String? = nil
 let fullName: String = "John Appleseed"
 let informalGreeting = "Hi \(nickname ?? fullName)"
+let informalGreeting2 = "Hi \(nickname ?? "yosep Park")"
+// '??'를 사용해서, 왼 쪽에 있는 옵셔널 값이 nil경우 오른쪽에 선언 된 값이 default값으로 사용된다.
+
+let optionNilString: String? = "hello"
+//let infromalGreeting3 = "Hi \(nickname ?? optionNilString)" - 둘 다 옵셔널로 사용할 경우 경고창이 뜨고 올바른 값이 뜨지 않는다. "Optional("hello")" 라고 표시된다.
 
 //: Switches support any kind of data and a wide variety of comparison operations—they aren’t limited to integers and tests for equality.
 //:
-let vegetable = "red pepper"
-switch vegetable {
-    case "celery":
+var vegetable = "red pepper"
+vegetable = "celery"
+switch vegetable { // 해당 값에서
+    case "celery": // 같은 case일 경우, 그 안의 구문이 실행된다.
         print("Add some raisins and make ants on a log.")
     case "cucumber", "watercress":
         print("That would make a good tea sandwich.")
-    case let x where x.hasSuffix("pepper"):
+    case let x where x.hasSuffix("pepper"): // 단어에 "pepper"가 들어간다면 x로 받는다는 것 같다.
         print("Is it a spicy \(x)?")
-    default:
+    default: // Switch 구문에서 default 값이 없으면 오류가 발생한다.
         print("Everything tastes good in soup.")
 }
 
@@ -68,7 +90,7 @@ let interestingNumbers = [
     "Square": [1, 4, 9, 16, 25],
 ]
 var largest = 0
-for (_, numbers) in interestingNumbers {
+for (_, numbers) in interestingNumbers { // Prime, Fibonacci, Square 과 같은 keys를 제외하고, value만 취급해서 Numbers에 넣은 다음, numbers안의 number를 가지고 최대값 구하기.
     for number in numbers {
         if number > largest {
             largest = number
@@ -77,6 +99,20 @@ for (_, numbers) in interestingNumbers {
 }
 print(largest)
 
+let interestingNumbers2 = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest2 = 0
+for (keysName, numbers) in interestingNumbers2 {
+    for number in numbers {
+        if number > largest2 {
+            largest2 = number
+        }
+    }
+}
+print(largest2)
 //: - Experiment:
 //: Replace the `_` with a variable name, and keep track of which kind of number was the largest.
 //:
@@ -93,15 +129,24 @@ repeat {
     m *= 2
 } while m < 100
 print(m)
+// while을 사용하는 두 가지 구문.
+// 참일 때 반복되고, 거짓이 되면 중단.
 
 //: You can keep an index in a loop by using `..<` to make a range of indexes.
 //:
 var total = 0
-for i in 0..<4 {
+for i in 0..<4 { // 0부터 4 이전까지
     total += i
 }
 print(total)
 
+total = 0
+for i in 0...4 {
+    total += i
+}
+print(total)
+
+// 상한 값을 생략하는 범위를 만들려면 '..<'(n보다 작다) 를 사용하고 두 값을 모두 포함하는 범위를 만들려면 '...'(n보다 작거나 같다) 을 사용합니다.
 //: Use `..<` to make a range that omits its upper value, and use `...` to make a range that includes both values.
 //:
 
