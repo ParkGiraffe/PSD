060844ef0d6b3bf57c510b1f8375b0cf03edc3a6
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-05 15:54:27 +0900

    feat: Enumerations and Structures 장 학습완료

--- swift/swift_study/swift_official_docs/GuidedTour.playground/Pages/Enumerations and Structures.xcplaygroundpage/Contents.swift
+++ swift/swift_study/swift_official_docs/GuidedTour.playground/Pages/Enumerations and Structures.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 //:
 //: Use `enum` to create an enumeration. Like classes and all other named types, enumerations can have methods associated with them.
 //:
-enum Rank: Int { // Int를 원시값(rawValue)을 가지도록 정의
+enum Rank: Int { // Int를 원시값(rawValue)을 가지도록 정의. String과 floating point number도 사용할 수 있다.
     // 0을 기점으로, 그 이후에 나오는 케이스들은 자동으로 1씩 더해져서 rawValue를 가지는 것으로 보인다.
     case ace = 1 // 다만 원한다면, 초기값을 임의로 지정할 수 있다.
     case two, three, four, five, six, seven, eight, nine, ten
@@ -33,12 +33,23 @@ let aceRawValue = ace.rawValue
 //:
 //: Use the `init?(rawValue:)` initializer to make an instance of an enumeration from a raw value. It returns either the enumeration case matching the raw value or `nil` if there’s no matching `Rank`.
 //:
-if let convertedRank = Rank(rawValue: 3) {
+if let convertedRank = Rank(rawValue: 6) {
     let threeDescription = convertedRank.simpleDescription()
+    print(convertedRank)
 }
 
+if let convertedRank2 = Rank(rawValue: 11) {
+    let printedDescription = convertedRank2.simpleDescription()
+    print(convertedRank2)
+}
+
+if let convertedRank3 = Rank(rawValue: 16) { //no matching Rank => nil
+    let printedDescription = convertedRank3.simpleDescription()
+    print(convertedRank3)
+}
 //: The case values of an enumeration are actual values, not just another way of writing their raw values. In fact, in cases where there isn’t a meaningful raw value, you don’t have to provide one.
 //:
+// enum은 원시 값을 가지지 않을 수도 있다.
 enum Suit {
     case spades, hearts, diamonds, clubs
 
@@ -54,9 +65,26 @@ enum Suit {
                 return "clubs"
         }
     }
+    
+    //experiment 추가
+    func color() -> String {
+        switch self {
+            case .clubs, .spades:
+                return "black"
+            case .hearts, .diamonds:
+                return "red"
+        }
+    }
+    
 }
 let hearts = Suit.hearts
 let heartsDescription = hearts.simpleDescription()
+let heartsColor = hearts.color()
+
+let clubs: Suit = .clubs // ':'앞에 쓰여서, Enum을 예측할 수 있다면 Enum의 이름을 생략할 수도 있다.
+
+//  abbreviated : 생략된
+
 
 //: - Experiment:
 //: Add a `color()` method to `Suit` that returns “black” for spades and clubs, and returns “red” for hearts and diamonds.
@@ -65,6 +93,9 @@ let heartsDescription = hearts.simpleDescription()
 //:
 //: If an enumeration has raw values, those values are determined as part of the declaration, which means every instance of a particular enumeration case always has the same raw value. Another choice for enumeration cases is to have values associated with the case—these values are determined when you make the instance, and they can be different for each instance of an enumeration case. You can think of the associated values as behaving like stored properties of the enumeration case instance. For example, consider the case of requesting the sunrise and sunset times from a server. The server either responds with the requested information, or it responds with a description of what went wrong.
 //:
+// 연관 값(Associated Values)을 가지는 Enum
+// Enum은 연관 값을 가질 수 있다.
+
 enum ServerResponse {
     case result(String, String)
     case failure(String)
@@ -73,12 +104,22 @@ enum ServerResponse {
 let success = ServerResponse.result("6:00 am", "8:09 pm")
 let failure = ServerResponse.failure("Out of cheese.")
 
+
+// 연관값을 접근하기 위한 방법으로는 switch가 있다.
 switch success {
     case let .result(sunrise, sunset):
         print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
     case let .failure(message):
         print("Failure...  \(message)")
 }
+switch failure {
+    case let .result(sunrise, sunset):
+        print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+    case let .failure(message):
+        print("Failure...  \(message)")
+}
+
+
 
 //: - Experiment:
 //: Add a third case to `ServerResponse` and to the switch.
@@ -87,19 +128,36 @@ switch success {
 //:
 //: Use `struct` to create a structure. Structures support many of the same behaviors as classes, including methods and initializers. One of the most important differences between structures and classes is that structures are always copied when they’re passed around in your code, but classes are passed by reference.
 //:
+// class와 struct는 유사해보이지만, 가장 큰 차이는
+// class는 call by reference
+// struct는 call by value 라는 점이다.
+
 struct Card {
     var rank: Rank
     var suit: Suit
     func simpleDescription() -> String {
         return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
     }
+    
+    func createDeck() -> [Card] {
+        var n = 1
+        var deck = [Card]()
+        let suits = [Suit.spades, Suit.hearts, Suit.diamonds, Suit.clubs]
+        while let rank = Rank(rawValue: n) { // rawValue가 일정 이상 넘어가면, 범위에 벗어날 경우 nil을 반환함.
+            for suit in suits {
+                deck.append(Card(rank: rank, suit: suit))
+            }
+            n += 1
+        }
+        return deck
+    }
+
 }
 let threeOfSpades = Card(rank: .three, suit: .spades)
 let threeOfSpadesDescription = threeOfSpades.simpleDescription()
-
+let deck = threeOfSpades.createDeck()
 //: - Experiment:
 //: Write a function that returns an array containing a full deck of cards, with one card of each combination of rank and suit.
 //:
-
 
 //: [Previous](@previous) | [Next](@next)
