ad0c0de4c80cbb7396849d2b921cb70a2cc5ae97
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-11 13:38:58 +0900

    feat : 프로그래머스 1단계 통과

--- swift/swift_study/programmers_note/Level1_note.playground/Contents.swift
+++ swift/swift_study/programmers_note/Level1_note.playground/Contents.swift
@@ -53,3 +53,47 @@ func solution3(_ n:Int) -> Int {
 
 let result1 = solution3(504920)
 
+
+
+//이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+//별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a1, b) = (n[0], n[1])
+//
+//for i in 0 ..< b {
+//    for j in 0 ..< a1 {
+//        print("*", terminator: "")
+//    }
+//    print()
+//}
+
+
+
+
+
+// 두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+func solution4(_ left:Int, _ right:Int) -> Int {
+    var count: Int = 0
+    var result: Int = 0
+    for i in left ... right {
+        count = 0
+        for n in 1 ... i {
+            if i%n == 0 {
+                count += 1
+            }
+            
+        }
+        if count % 2 == 0 {
+            result += i
+        } else {
+            result -= i
+        }
+    }
+    
+    return result
+}
+
+solution4(13,17) //43
+solution4(24,27) //52
