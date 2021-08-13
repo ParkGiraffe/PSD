2aa8b2f30fd399ebdf37ba8446cd9a84d8758662
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-13 18:14:29 +0900

    feat: Section2 완료

--- swift/swift_study/swift_official_docs/Swift_UI_Tutorials_Lansmarks/Landmarks/Landmarks/ContentView.swift
+++ swift/swift_study/swift_official_docs/Swift_UI_Tutorials_Lansmarks/Landmarks/Landmarks/ContentView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 struct ContentView: View { // View protocol.
     // describe the view's content and layout.
     var body: some View {
-        Text("Hello, ParkGiraffe")
-            .padding()
+        // To customize a SwiftUI view, you call methods called modifiers.
+        // .font, .padding,.foregroundColor 같은 것들을 Modifier라고 한다.
+        // 오른쪽에 뜨는 Xcode modifier을 통해서 수정할 수도 있다.
+        // Text를 cmd + 클릭해서, 'Show SwiftUI Inspector을 선택해도 된다.
+        Text("Turtle Rock")
+            .font(.title) // title : This applies the system font to the text so that it responds correctly to the user's preferred font sizes and settings.
+            
     }
 }
 
