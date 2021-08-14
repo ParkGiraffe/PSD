640332be0a44794bc3e4405b7089fd08e432ef5b
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-14 15:06:00 +0900

    feat: Section3 학습 완료

--- swift/swift_study/swift_official_docs/Swift_UI_Tutorials_Lansmarks/Landmarks/Landmarks/ContentView.swift
+++ swift/swift_study/swift_official_docs/Swift_UI_Tutorials_Lansmarks/Landmarks/Landmarks/ContentView.swift
@@ -14,8 +14,18 @@ struct ContentView: View { // View protocol.
         // .font, .padding,.foregroundColor 같은 것들을 Modifier라고 한다.
         // 오른쪽에 뜨는 Xcode modifier을 통해서 수정할 수도 있다.
         // Text를 cmd + 클릭해서, 'Show SwiftUI Inspector을 선택해도 된다.
-        Text("Turtle Rock")
-            .font(.title) // title : This applies the system font to the text so that it responds correctly to the user's preferred font sizes and settings.
+        VStack(alignment: .leading) { //vertical
+            Text("Turtle Rock")
+                .font(.title)// title : This applies the system font to the text so that it responds correctly to the user's preferred font sizes and settings.
+            HStack { // horizontal
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer() // A spacer expands to make its containing view use all of the space of its parent view, instead of having its size defined only by its contents.
+                Text("Califonia")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
             
     }
 }
