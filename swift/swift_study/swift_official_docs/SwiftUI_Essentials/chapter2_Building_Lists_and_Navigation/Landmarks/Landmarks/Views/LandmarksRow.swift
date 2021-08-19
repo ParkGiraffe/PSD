08e354796fbe994c5a1977a4b9b950469f63595e
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-19 21:52:34 +0900

    feat: Building Lists and Navigation chapter의 Section2 시작

---
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter2_Building_Lists_and_Navigation/Landmarks/Landmarks/Views/LandmarksRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksRow.swift
+//  Landmarks
+//
+//  Created by 박요셉 on 2021/08/19.
+//
+
+import SwiftUI
+
+struct LandmarksRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarksRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarksRow()
+    }
+}
