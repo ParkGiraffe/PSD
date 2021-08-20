45ea892d486beb4a2bfccac4f91cec27233567d8
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-20 21:44:07 +0900

    feat: Building Lists and Navigation chapter의 Section3 완료

--- swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter2_Building_Lists_and_Navigation/Landmarks/Landmarks/Views/LandmarksRow.swift
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter2_Building_Lists_and_Navigation/Landmarks/Landmarks/Views/LandmarksRow.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct LandmarksRow: View {
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
     }
 }
 
 struct LandmarksRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarksRow()
+        LandmarksRow(landmark: landmarks[0])
     }
 }
