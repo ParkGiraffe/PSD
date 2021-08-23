aa28ea43e40b31b2ae3535897c8eaa715f359dca
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-23 15:42:20 +0900

    feat: section 6 시작

---
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter2_Building_Lists_and_Navigation/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 박요셉 on 2021/08/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
