1f6e3ceb8f04a6c1fc80bf5cc65115efe109e897
ParkGiraffe <op5321@soongsil.ac.kr>
2021-08-25 18:53:59 +0900

    feat: Building Lists and Navigation의 section 8,9 완료

--- swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter2_Building_Lists_and_Navigation/Landmarks/Landmarks/Views/LandmarkList.swift
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter2_Building_Lists_and_Navigation/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -24,7 +24,13 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
-            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
     }
 }
+
+//ForEach operates on collections the same way as the list, which means you can use it anywhere you can use a child view, such as in stacks, lists, groups, and more. When the elements of your data are simple value types — like the strings you’re using here — you can use \.self as key path to the identifier.
+//ForEach는 컬렉션에서 리스트와 동일한 방식으로 작동하므로 스택, 목록, 그룹 등 하위 보기를 사용할 수 있는 모든 위치에서 사용할 수 있습니다. 데이터의 요소가 여기서 사용하는 문자열과 같은 단순한 값 유형인 경우 \.self를 식별자의 키 경로로 사용할 수 있습니다.
