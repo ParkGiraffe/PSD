8cdec09735ca32472d62f6464dc1868d818bbccb
ParkGiraffe <op5321@soongsil.ac.kr>
2021-09-01 14:10:54 +0900

    feat: Section6 시작

---
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter3_Handling_User_Input/Landmarks/Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by 박요셉 on 2021/09/01.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton()
+    }
+}
