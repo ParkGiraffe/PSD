c1efb92a1b9e8286931b5797f053bda69b85d0f7
ParkGiraffe <op5321@soongsil.ac.kr>
2021-09-03 18:07:36 +0900

    feat: Section2 시작

---
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter4_Drawing_Paths_and_Shapes/Landmarks/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by 박요셉 on 2021/09/03.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
