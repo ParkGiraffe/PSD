448eb455b95c325139e72124c0b84f3076b6a133
ParkGiraffe <op5321@soongsil.ac.kr>
2021-09-04 20:23:50 +0900

    fix: Section2 gradient  오류 수정

--- swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter4_Drawing_Paths_and_Shapes/Landmarks/Landmarks/Views/BadgeBackground.swift
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter4_Drawing_Paths_and_Shapes/Landmarks/Landmarks/Views/BadgeBackground.swift
@@ -56,6 +56,7 @@ struct BadgeBackground: View {
         .aspectRatio(1, contentMode: .fit)
         
     }
+    // gradient 값 지정
     static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
     static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
