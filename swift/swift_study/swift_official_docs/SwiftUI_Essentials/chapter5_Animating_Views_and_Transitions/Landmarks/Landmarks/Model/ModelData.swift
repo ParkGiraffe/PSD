8e9458fe0793e4bfc4eea79f82c54318a66a78bb
ParkGiraffe <op5321@soongsil.ac.kr>
2021-09-11 18:25:14 +0900

    feat: Animating Views and Transitions chapter 시작

---
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter5_Animating_Views_and_Transitions/Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,42 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by 박요셉 on 2021/08/18.
+//
+
+import Foundation
+import Combine
+
+final class ModelData: ObservableObject { // SwiftUI subscribes to your observable object, and updates any views that need refreshing when the data changes.
+    
+    // create an array initialized with landmarks from a file.
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    // An observable object needs to publish any changes to its data, so that its subscribers can pick up the change.
+}
+
+
+
+// Create a load(_:) method that fetches JSON data with a given name from the app’s main bundle.
+// The load method relies on the return type’s conformance to the Codable protocol.
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
---
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter5_Animating_Views_and_Transitions/Landmarks/Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by 박요셉 on 2021/09/06.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
