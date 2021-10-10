943a08b5f3bb61ee63b5e1ce6fbb3050ce1f9d02
ParkGiraffe <op5321@soongsil.ac.kr>
2021-10-10 12:16:12 +0900

    feat: section5 완료

--- swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter6_Composing_Complex_Interfaces/Landmarks/Landmarks/Views/CategoryRow.swift
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter6_Composing_Complex_Interfaces/Landmarks/Landmarks/Views/CategoryRow.swift
@@ -23,7 +23,9 @@ struct CategoryRow: View {
                 HStack(alignment: .top, spacing: 0) {
                 ForEach(items) {
                     landmark in
-                    CategoryItem(landmark: landmark)
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        CategoryItem(landmark: landmark)
+                        }
                     }
                 }
                 
