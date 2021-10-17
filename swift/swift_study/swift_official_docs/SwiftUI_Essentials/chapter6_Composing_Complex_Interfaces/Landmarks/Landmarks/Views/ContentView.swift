78a54dc7d7414e92113e545e9fd12422a2d818e3
ParkGiraffe <op5321@soongsil.ac.kr>
2021-10-17 20:34:19 +0900

    fix: 주석 수정 및 파일 위치 변경

--- swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter6_Composing_Complex_Interfaces/Landmarks/Landmarks/Views/ContentView.swift
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter6_Composing_Complex_Interfaces/Landmarks/Landmarks/Views/ContentView.swift
@@ -40,6 +40,6 @@ struct ContentView_Previews: PreviewProvider {
         // Update the ContentView preview to add the model object to the environment, which makes the object available to any subview
         // A preview fails if any subview requires a model object in the environment, but the view you are previewing doesn’t have the environmentObject(_:) modifier.
         // 여기에서 environment로 model object를 전달해준다고 설정하지 않으면, model object를 필요로하는 하위 view에서 오류가 발생하는 것 같음.
-        // 그래서 LandmarksApp.Swift에서 ModelData 인스턴스를 불러옴.
+        // 그래서 LandmarksApp.Swift에서 ModelData 인스턴스를 불러옴
     }
 }
