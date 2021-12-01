4ccd9c0caea079f3b9de61c0c6c85c490862bcd3
ParkGiraffe <op5321@soongsil.ac.kr>
2021-12-01 22:49:21 +0900

    feat: swift UI - Working With UI section 4 완료

--- swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter7_Working_with_UI_Controls/Landmarks/Views/Profiles/ProfileHost.swift
+++ swift/swift_study/swift_official_docs/SwiftUI_Essentials/chapter7_Working_with_UI_Controls/Landmarks/Views/Profiles/ProfileHost.swift
@@ -17,6 +17,17 @@ struct ProfileHost: View {
         VStack(alignment: .leading, spacing: 20) {
             
             HStack {
+                if editMode?.wrappedValue == .active {
+                    if #available(iOS 15.0, *) {
+                        //Button 컴포넌트가 IOS 15.0부터 지원된다는 거 같은데 추가조사 필요.
+                        Button("Cancel", role: .cancel) {
+                            draftProfile = modelData.profile
+                            editMode?.animation().wrappedValue = .inactive
+                        }
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                }
                 Spacer()
                 EditButton()
                 // EditButton toggles the envrionment's editMode value on and off.
@@ -26,6 +37,14 @@ struct ProfileHost: View {
             
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        //edit버튼을 누르면 edit페이지 안의 draftProfile 변수에 현재 계정데이터를 덮어 씀
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        //edit page에서 Done 버튼을 누르고 나오면, edit page 안의 draftProfile 데이터를 바깥 modelData.profile에 집어 넣음.
+                        modelData.profile = draftProfile
+                    }
             }
             }
         .padding()
